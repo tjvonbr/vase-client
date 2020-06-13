785e71be6f1cdc772c0e2594a9047915c51f6275
tjvonbr <51134664+tjvonbr@users.noreply.github.com>
2020-06-12 23:57:11 -0500

    redesigned landing page and completed 'get started' button

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,18 +1,20 @@
 /** @jsx jsx */
 
 import React from 'react';
-import { jsx } from '@emotion/core';
-import { useTheme } from 'emotion-theming';
+import { jsx, keyframes } from '@emotion/core';
 import { Link } from 'react-router-dom';
 import { Button, Icon } from 'semantic-ui-react';
 import Banner from './Banner';
 
 function Landing() {
-  const theme = useTheme();
-  const size = '2.1rem';
-
   return (
-    <header>
+    <header
+      css={{
+        display: 'block',
+        overflow: 'hidden'
+      }}
+    >
+      <Banner />
       <div 
         css={{
           width: '100%',
@@ -20,6 +22,7 @@ function Landing() {
           background: 'linear-gradient(150deg, #53f 15%, #05d5ff 70%, #a6ffcb 94%)',
           transform: 'skewY(-10deg)',
           transformOrigin: '0',
+          zIndex: 0
         }}
       >
       </div>
@@ -31,10 +34,43 @@ function Landing() {
           top: '200px',
           left: '200px',
           color: '#fff',
-          border: '1px solid #fff'
         }}
       >
-        <h1>A new way to engage</h1>
+        <h1
+          css={{
+            letterSpacing: '2px'
+          }}
+        >A new way to engage</h1>
+        <p
+          css={{
+            color: 'rgb(217, 252, 255)',
+            fontSize: '17px',
+            lineHeight: '1.8'
+          }}
+        >
+          Vase is the smart approach to engaging with your neighbors.  See what others are up to on your block.  Check in to see what issues within your community need to be addressed.  Give your local officials an idea of what concerns you most.  Vase turns the web into a virtual town hall.  How will you help build your community?
+        </p>
+        <button
+          css={{
+            height: '40px',
+            border: 'none',
+            background: '#3ecf8e',
+            color: '#fff',
+            fontSize: '15px',
+            fontWeight: '600',
+            textShadow: '0 1px 3px rgba(36, 180, 126, .4)',
+            borderRadius: '4px',
+            padding: '0 14px',
+            boxShadow: '0 10px 6px rgba(50, 50, 93, .11)',
+            textTransform: 'uppercase',
+            '&:hover': {
+              cursor: 'pointer',
+              transform: 'translateY(1px)'
+            }
+          }}
+        >
+          Get Started
+        </button>
       </section>
 
     </header>
