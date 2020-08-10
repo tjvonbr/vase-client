0b445062afd4bd0051deace9046705a10bfa9c5b
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2020-08-09 19:35:57 -0500

    updated profile page--layout of list of issues

--- src/components/Community.js
+++ src/components/Community.js
@@ -1,88 +1,85 @@
 /** @jsx jsx */
 
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { Dimmer, Loader } from 'semantic-ui-react';
 import { jsx } from '@emotion/core';
 import axios from 'axios';
 import CommunityIssues from './CommunityIssues';
-import MostPopular from './MostPopular';
 import NavBar from './NavBar';
 import NoIssues from './NoIssues';
 
 function Community() {
+  const auth = useContext(AuthContext);
+  const { token } = auth.authState;
+  const { id, zipcode } = auth.authState.userInfo;
+  
   const [communityIssues, setCommunityIssues] = useState([]);
   const [userVotes, setUserVotes] = useState([])
-  const [mostPopular, setMostPopular] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchUserVotes();
     fetchCommunityIssues();
-  }, [userVotes]);
+    fetchUserVotes();
+  }, []);
 
-  const token = window.localStorage.getItem('token');
-  const zip = window.localStorage.getItem('zipcode');
-  const id = window.localStorage.getItem('id')
-
-  function fetchCommunityIssues() {
-    axios
-      .get(`http://localhost:4000/issues/zip/${zip}`, {
+  async function fetchCommunityIssues() {
+    try {
+    const { data } = await axios
+      .get(`http://localhost:4000/issues/zip/${zipcode}`, {
         headers: {
           Authorization: token
         }
       })
-      .then(response => {
-        setCommunityIssues(response.data);
-        setMostPopular(response.data.reduce((prev, current) => {
-          return (prev.upvotes > current.upvotes) ? prev : current
-        }));
-      })
-      .catch(error => {
-        console.log(error);
-      })
+      setCommunityIssues(data);
+      setLoading(false);
+      } catch (error) {
+        console.log(error)
+      }
   };
 
-  function fetchUserVotes() {
-    axios
+  async function fetchUserVotes() {
+    try {
+      const { data } = await axios
       .get(`http://localhost:4000/users/${id}/upvotes`, {
         headers: {
           Authorization: token
         }
       })
-      .then(response => {
-        setUserVotes(response.data)
-      })
-      .catch(error => console.log(error))
+      setUserVotes(data)
+      setLoading(false);
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  if (communityIssues < 1) {
-    return <NoIssues />
-  } else {
-    return (
-      <div>
-        <NavBar /> 
-        <div
-          css={{
-            margin: '20px 0px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <h3>Top concern in {zip}:</h3>
-          <MostPopular issue={mostPopular} />
-        </div>
-        <div
-          css={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <h3>All community concerns posted in {zip}:</h3>
-          <CommunityIssues issues={communityIssues} upvotes={userVotes} />
-        </div>
+  return (
+    <div>
+      <Dimmer active={ loading ? true : false }>
+        <Loader>Loading...</Loader>
+      </Dimmer>
+      <NavBar /> 
+      <div
+        css={{
+          margin: '50px 0px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
       </div>
-    )
-  }
-};
+      <div
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <h3>All community concerns posted in {zipcode}:</h3>
+        <CommunityIssues issues={communityIssues} upvotes={userVotes} />
+      </div>
+    </div>
+  )
+}
 
 export default Community;
